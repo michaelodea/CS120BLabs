1295a06d2487c78c1db10ead7a3f7716666d18b6
Michael O'Dea <mode001@wch136-22.cs.ucr.edu>
2020-04-08 14:42:09 -0700

    Added part 2 of CS100 Lab2

---
+++ main.c
@@ -0,0 +1,46 @@
+/*      Partner(s) Name & E-mail: Michael O'Dea, mode001@ucr.edu
+ *                     Lab Section: 025
+ *                     Assignment: Lab #2  Exercise #1
+ *                     Exercise Description: Garage open at night-- A garage door sensor connects to PA0 (1 means        *                     door open), and a light sensor connects to PA1 (1 means light is sensed). Write a program         *                     that illuminates an LED connected to PB0 (1 means illuminate) if the garage door is open          *                     at night.
+ *          
+ *                      I acknowledge all content contained herein, excluding template or example
+ *                      code, is my own original work.
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+					// Initialize output on PORTB to 0x00
+	unsigned char cntavail = 0x00; // Temporary variable to hold the value of cntavail
+	unsigned char spaceOne = 0x00; // Hold's value of 1st parking space
+	unsigned char spaceTwo = 0x00; // Hold's value of 2nd parking space
+	unsigned char spaceThree = 0x00; // Hold's value of 3rd parking space
+	unsigned char spaceFour = 0x00; // Hold's value of 4th parking space
+
+	while(1){
+		// 1) Read input
+		tmpA = PINA & 0x01;
+		tmpB = PINA & 0x02;
+		// 2) Perform computation
+		// if PA0 is 1, set PB1PB0 = 01, else = 10
+		if ((tmpA == 0x01) && (tmpB == 0x00))  { // True if PA0 is 1
+			tmpPortB = 0x01;
+							
+		}
+		else{
+			tmpPortB = 0x00;
+
+		}
+		
+		PORTB = tmpPortB;
+	}
+
+return 0;
+
+}
+
